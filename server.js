a16991d16788ba2101d14e13d8a576afbf6bcc44
atoullo <atoulloraimov@gmail.com>
2021-02-05 19:21:35 +0500

    some changes

--- server.js
+++ server.js
@@ -66,5 +66,5 @@ io.on('connection', socket => {
 
 })
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log(`Server running on ${PORT} port`))
+// const PORT = process.env.PORT || 3000
+// server.listen(PORT, () => console.log(`Server running on ${PORT} port`))
