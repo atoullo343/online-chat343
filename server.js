0ddfc47aa8b14b6ae23408e407ad59c918e4c5b6
atoullo343 <71008900+atoullo343@users.noreply.github.com>
2022-02-22 17:35:58 +0500

    Update server.js
    Some changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const formatMessage = require('./utils/messages')
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 
 const app = express()
